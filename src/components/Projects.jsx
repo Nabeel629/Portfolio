bd5328c735ea0c39417e27dff2f0004065461c21
Nabeel Akram <professionalworks629@gmail.com>
2026-02-04 18:35:06 +0500

    link add on the projects cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -42,42 +42,42 @@ import xeemart from "../assets/wordpress_website_screenShot/xeemart-pk.png";
 /* ================= PROJECT DATA ================= */
 const projectsData = [
   /* SHOPIFY */
-  { id: 1, title: "Davis & Co Aesthetics", category: "shopify", image: davisandcoaesthetics },
-  { id: 2, title: "Bijju Online", category: "shopify", image: bijjuonline },
-  { id: 3, title: "Brandin PK", category: "shopify", image: brandinpk },
-  { id: 4, title: "Colourway", category: "shopify", image: colourway },
-  { id: 5, title: "Derm Pharma", category: "shopify", image: dermpharma },
-  { id: 6, title: "Gowara Foods", category: "shopify", image: gowarafoods },
-  { id: 7, title: "Laxariah", category: "shopify", image: laxariah },
-  { id: 8, title: "Origin Pharma", category: "shopify", image: originpharma },
-  { id: 9, title: "Rosewood Furnish", category: "shopify", image: rosewoodfurnish },
-  { id: 10, title: "Sizu Cchero", category: "shopify", image: sizucchero },
-  { id: 11, title: "Y Sons PK", category: "shopify", image: ysons },
-  { id: 12, title: "Zainab Zari", category: "shopify", image: zainabzari },
-  { id: 13, title: "Zens Org", category: "shopify", image: zensorg },
+  { id: 1, title: "Davis & Co Aesthetics", category: "shopify", image: davisandcoaesthetics ,url:"https://davisandcoaesthetics.com/" },
+  { id: 2, title: "Bijju Online", category: "shopify", image: bijjuonline ,url:"https://bijjuonline.com/" },
+  { id: 3, title: "Brandin PK", category: "shopify", image: brandinpk ,url:"https://brandinpk.com/" },
+  { id: 4, title: "Colourway", category: "shopify", image: colourway  ,url:"https://colourway.pk/?srsltid=AfmBOopDII6BN9ydoOByd3WP8lGILrySxon52Ns02pu5DbBcj6guH3p9"},
+  { id: 5, title: "Derm Pharma", category: "shopify", image: dermpharma  ,url:"https://dermpharma.pk/"},
+  { id: 6, title: "Gowara Foods", category: "shopify", image: gowarafoods  ,url:"https://www.gowarafoods.com/"},
+  { id: 7, title: "Laxariah", category: "shopify", image: laxariah  ,url:"https://laxariah.com/"},
+  { id: 8, title: "Origin Pharma", category: "shopify", image: originpharma ,url:"https://originpharma.org/" },
+  { id: 9, title: "Rosewood Furnish", category: "shopify", image: rosewoodfurnish ,url:"https://rosewoodfurnish.com/" },
+  { id: 10, title: "Sizu Cchero", category: "shopify", image: sizucchero  ,url:"https://www.sizucchero.com/"},
+  { id: 11, title: "Y Sons PK", category: "shopify", image: ysons ,url:"https://ysons.com.pk/" },
+  { id: 12, title: "Zainab Zari", category: "shopify", image: zainabzari  ,url:"https://www.zainabzari.pk/"},
+  { id: 13, title: "Zens Org", category: "shopify", image: zensorg  ,url:"https://zensorg.com/"},
 
   /* WORDPRESS */
-  { id: 14, title: "ZperSoft", category: "wordpress", image: zpersoft },
-  { id: 15, title: "Asphalt Attire", category: "wordpress", image: asphaltattire },
-  { id: 16, title: "Baghbani", category: "wordpress", image: baghbani },
-  { id: 17, title: "Dermatology Clinic", category: "wordpress", image: dermatology },
-  { id: 18, title: "DeveloperTech", category: "wordpress", image: developertech },
-  { id: 19, title: "Eco Grocer", category: "wordpress", image: ecogrocer },
-  { id: 20, title: "Furry's Wellness", category: "wordpress", image: furryswellness },
-  { id: 21, title: "GI Mechanical", category: "wordpress", image: gimechanical },
-  { id: 22, title: "Graceful Hijabs", category: "wordpress", image: gracefulhijabs },
-  { id: 23, title: "Grind & Gains", category: "wordpress", image: grindandgains },
-  { id: 24, title: "Mandy Store", category: "wordpress", image: mandystore },
-  { id: 25, title: "PNY Training", category: "wordpress", image: pnyc },
-  { id: 26, title: "Polani CA", category: "wordpress", image: polani },
-  { id: 27, title: "Prime Footwears", category: "wordpress", image: primefootwears },
-  { id: 28, title: "RTV Outdoors", category: "wordpress", image: rtvoutdoors },
-  { id: 29, title: "The Fragrances House", category: "wordpress", image: thefragranceshouse },
-  { id: 30, title: "The Urban Strap", category: "wordpress", image: theurbanstrap },
-  { id: 31, title: "Upfront Security Services", category: "wordpress", image: upfrontsecurityservices },
-  { id: 32, title: "Village Crafting", category: "wordpress", image: villagecrafting },
-  { id: 33, title: "Wiser Soft Solutions", category: "wordpress", image: wisersoftsol },
-  { id: 34, title: "Xeemart PK", category: "wordpress", image: xeemart },
+  { id: 14, title: "ZperSoft", category: "wordpress", image: zpersoft  ,url:"https://zpersoft.com/"},
+  { id: 15, title: "Asphalt Attire", category: "wordpress", image: asphaltattire  ,url:"#"},
+  { id: 16, title: "Baghbani", category: "wordpress", image: baghbani  ,url:"https://baghbani.xyz/"},
+  { id: 17, title: "Dermatology Clinic", category: "wordpress", image: dermatology  ,url:"https://dermatology.pk/"},
+  { id: 18, title: "DeveloperTech", category: "wordpress", image: developertech ,url:"https://developertech.net/" },
+  { id: 19, title: "Eco Grocer", category: "wordpress", image: ecogrocer ,url:"https://ecogrocer.store/" },
+  { id: 20, title: "Furry's Wellness", category: "wordpress", image: furryswellness ,url:"https://furryswellness.com/" },
+  { id: 21, title: "GI Mechanical", category: "wordpress", image: gimechanical  ,url:"https://gimechanical.ca/"},
+  { id: 22, title: "Graceful Hijabs", category: "wordpress", image: gracefulhijabs ,url:"https://gracefulhijabs.com/" },
+  { id: 23, title: "Grind & Gains", category: "wordpress", image: grindandgains  ,url:"https://grindandgains.com/"},
+  { id: 24, title: "Mandy Store", category: "wordpress", image: mandystore  ,url:"https://mandystore.online/"},
+  { id: 25, title: "PNY Training", category: "wordpress", image: pnyc ,url:"https://pnyc.pk/" },
+  { id: 26, title: "Polani CA", category: "wordpress", image: polani  ,url:"https://polani.ca/"},
+  { id: 27, title: "Prime Footwears", category: "wordpress", image: primefootwears  ,url:"https://primefootwears.com/"},
+  { id: 28, title: "RTV Outdoors", category: "wordpress", image: rtvoutdoors,url:"https://rtvoutdoors.net/" },
+  { id: 29, title: "The Fragrances House", category: "wordpress", image: thefragranceshouse  ,url:"https://thefragranceshouse.com/"},
+  { id: 30, title: "The Urban Strap", category: "wordpress", image: theurbanstrap  ,url:"https://theurbanstrap.com/"},
+  { id: 31, title: "Upfront Security Services", category: "wordpress", image: upfrontsecurityservices ,url:"https://upfrontsecurityservices.com/" },
+  { id: 32, title: "Village Crafting", category: "wordpress", image: villagecrafting ,url:"https://villagecrafting.com/" },
+  { id: 33, title: "Wiser Soft Solutions", category: "wordpress", image: wisersoftsol  ,url:"https://wisersoftsol.com/"},
+  { id: 34, title: "Xeemart PK", category: "wordpress", image: xeemart ,url:"https://xeemart.pk/" },
 ];
 
 export default function Projects() {
@@ -116,12 +116,21 @@ export default function Projects() {
       {/* PROJECT CARDS */}
       <div className="projects-grid">
         {filteredProjects.slice(0, visibleCount).map(project => (
-          <div className="project-card" key={project.id}>
-            <div className="project-image">
-              <img src={project.image} alt={project.title} />
-            </div>
-            <h3>{project.title}</h3>
-          </div>
+          
+          <a
+  href={project.url}
+  target="_blank"
+  rel="noopener noreferrer"
+  className="project-link"
+>
+  <div className="project-card">
+    <div className="project-image">
+      <img src={project.image} alt={project.title} />
+    </div>
+    <h3>{project.title}</h3>
+  </div>
+</a>
+
         ))}
       </div>
 
